86e8e206373ed73ee666d0b3126405e7f0cfc8cf
gert <gert@localhost.localdomain>
2010-07-28 23:26:32 +0200

    added simpleroom.erl

---
+++ simpleroom.erl
@@ -0,0 +1,51 @@
+%%% @author Gert  Meulyzer <@G3rtm on Twitter>
+%%% @copyright (C) 2010, Gert  Meulyzer
+%%% @doc
+%%% Simple room.
+%%% Going to try and make a room without OTP first, so I fully understand everything that is happening.
+%%% Later this will be ported to a gen_server so we can use all the OTP stuff.
+%%% @end
+%%% Created : 28 Jul 2010 by Gert  Meulyzer <@G3rtm on Twitter>
+
+-module(simpleroom).
+
+-compile(export_all).
+
+-record(room, {name,
+	       exits=[],
+	       desc=[],
+	       npcs=[],
+	       objects=[],
+	       players=[],
+	       messages=["room msg 1","room msg 2"]
+	      }).
+
+init() ->
+    loop(#room{name="testroom1"}).
+
+loop(State) ->
+    receive
+	{enter,PlayerPid} ->
+	    Players = State#room.players,
+	    NewPlayers = [PlayerPid|Players],
+	    NewState = State#room{players=NewPlayers},
+	    PlayerPid ! {ok,State#room.name},
+	    io:format("~s~n",["looping from enter"]),
+	    loop(NewState);
+	{stop} ->
+	    {ok,stopped};
+	_ -> ok
+    after 15000 ->
+	    room_message(State),
+	    io:format("~s~n",["looping from after"]),
+	    loop(State)
+    end.
+
+room_message(#room{players=P,messages=M}) ->
+    Message = lists:nth(random:uniform(length(M)),M),
+    [Player ! Message || Player <- P].
+
+    
+    
+    
+    
