9a0fbe375b1e8188c3e73e3c5bddef789a09304c
Gert Meulyzer <Gert Meulyzer@localhost>
2010-08-03 23:28:41 +0200

    added comment

--- area.erl
+++ area.erl
@@ -7,17 +7,20 @@
 %%% @end
 %%% Created : 24 May 2010 by Gert M <cel357@gmail.com>
 
--module(area).
+-module(area). 
 -compile(export_all).  %% for now
 
 %% area file = erlang list describing the layout of the rooms.
 %% load area file into dict
 %% add appropriate exits to the rooms
 %% spawn all the rooms needed.
+%% the rooms loaded have the form: {{x,y,z},"roomfile"}
+%% where roomfile is without the extension.
 
 load(Filename) ->
     {ok,RoomList} = file:consult(Filename),
     RoomList.
+
 
 %% for testing:
 %% area:load("/home/gert/src/explore/erlang/musq/areas/source.area").
