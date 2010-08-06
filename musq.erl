c9707a265fe8f05cdfbc9758f2388fe1678a73fa
Gert Meulyzer <gert@dzine.be>
2010-08-06 16:04:56 +0200

    placeholder musq.erl file

---
+++ musq.erl
@@ -0,0 +1,12 @@
+%%% @author Gert Meulyzer <@G3rtm on Twitter>
+%%% @copyright (C) 2010, Gert Meulyzer
+%%% @doc
+%%% This is the 'main' module. This should start every thing else.
+%%% aka. This is the file you load when you want to start the game.
+%%% @end
+%%% Created :  6 Aug 2010 by Gert Meulyzer <@G3rtm on Twitter>
+
+-module(musq).
+
+
+
