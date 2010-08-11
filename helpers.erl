797cdd0be808f55475a4d256287b6bbf7c59a9e1
Gert Meulyzer <cel357@gmail.com>
2010-08-11 22:02:00 +0200

    added helpers.erl

---
+++ helpers.erl
@@ -0,0 +1,15 @@
+%%% @author Gert  Meulyzer <@G3rtm on Twitter>
+%%% @copyright (C) 2010, Gert  Meulyzer
+%%% @doc
+%%% Helper functions module. For random functions not fitting anywhere.
+%%% @end
+%%% Created : 11 Aug 2010 by Gert  Meulyzer <@G3rtm on Twitter>
+
+-module(helpers).
+
+-compile(export_all).
+
+clean_room_name(RoomName) ->
+    re:replace(RoomName," ","_").
+
+
