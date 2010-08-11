7c798387d5a2f6bc720306c10db6b28e4c7c6cc3
Gert Meulyzer <cel357@gmail.com>
2010-08-11 22:02:30 +0200

    comment adjustment

--- helpers.erl
+++ helpers.erl
@@ -1,7 +1,7 @@
 %%% @author Gert  Meulyzer <@G3rtm on Twitter>
 %%% @copyright (C) 2010, Gert  Meulyzer
 %%% @doc
-%%% Helper functions module. For random functions not fitting anywhere.
+%%% Helper functions module. For random functions not fitting anywhere else.
 %%% @end
 %%% Created : 11 Aug 2010 by Gert  Meulyzer <@G3rtm on Twitter>
 
