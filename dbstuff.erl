0e2b00bf6c60e3d0862ac1a6a6120193181df15d
Gert Meulyzer <gert@dzine.be>
2010-08-20 16:29:29 +0200

    added some mnesia stuff to dbstuff

--- dbstuff.erl
+++ dbstuff.erl
@@ -5,12 +5,26 @@
 %% little module to initialize db stuff
 %% ---
 
+-include("records.hrl").
+
 start() ->
-    mnesia:create_schema([node()]),
+    mnesia:create_schema([node()]), %% we probably don't need this every time.
     application:start(mnesia).
+
+init() ->
+    mnesia:create_tables(player,[{attributes, record_info(fields,player)},
+				 {disc_copies,[node()]},
+				 {type, set}]).
 
 stop() ->
     application:stop(mnesia).
+
+
+get_player(PlayerPid) ->
+    mnesia
+
+save_player(PlayerPid) ->
+    ok.
 
 
 
