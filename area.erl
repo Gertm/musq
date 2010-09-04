cc02faad961f61def741cd67cc0e41a05d467d18
Gert Meulyzer <cel357@gmail.com>
2010-09-04 10:33:09 +0200

    tested my fix-commas emacs function on this file.. nice result :D

--- area.erl
+++ area.erl
@@ -14,21 +14,22 @@
 %% load area file into dict
 %% add appropriate exits to the rooms
 %% spawn all the rooms needed.
-%% the rooms loaded have the form: {{x,y,z},"roomfile"}
+%% the rooms loaded have the form: {{x, y, z}, "roomfile"}
 %% where roomfile is the filename of the room
 
 load(Filename) ->
-    {ok,RoomList} = file:consult(Filename),
-    PosPidPairs = [ {Pos,just_the_pid(room:load(X))} || {Pos, X} <- RoomList ],
+    {ok, RoomList} = file:consult(Filename), 
+    PosPidPairs = [ {Pos, just_the_pid(room:load(X))} || {Pos, X} <- RoomList ], 
     
 
 just_the_pid({ok, Pid}) ->
     Pid.
 
-is_exit_of({{X1, Y1, Z1}, Pid1},{{X2, Y2, Z2}, Pid2}) ->
+is_exit_of({{X1, Y1, Z1}, Pid1}, {{X2, Y2, Z2}, Pid2}) ->
     ok.
     
-
+some_function(X, Y) ->
+    ok.
 
 %% for testing:
 %% area:load("/home/gert/src/musq/areas/source.area").
