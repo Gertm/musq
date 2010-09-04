e013fb61110c27a2f295fd482a3101f8c385f539
Randy Voet <randyv03t@gmail.com>
2010-09-04 17:55:49 +0200

    implementation of wrap function (split text into lines)

--- helpers.erl
+++ helpers.erl
@@ -10,10 +10,47 @@
 -include_lib("eunit/include/eunit.hrl").
 
 clean_name(Name) ->
-    re:replace(Name," ","_",[{return, list}]).
+    re:replace(Name, " ", "_", [{return, list}]).
 
 clean_name_test_() ->
     [?_assert(clean_name("test 1") =:= "test_1")].
+
+wrap(String, Columns) ->
+    Words = split_words(String, $ ),
+    add_words(Words, [""], Columns).
+
+wrap_test_() ->
+    [?_assert(wrap("", 10) =:= [""]),
+     ?_assert(wrap("aa bb cc dd", 20) =:= ["aa bb cc dd"]),
+     ?_assert(wrap("aa bb cc dd", 8) =:= ["aa bb cc", "dd"]),
+     ?_assert(wrap("aa bb cc dd", 4) =:= ["aa", "bb", "cc", "dd"])].
+
+split_words(String, Separator) ->
+    split_words(String, Separator, "", []).
+
+split_words(String, Separator, CurrentWord, Words) ->
+    case String of
+	"" -> lists:reverse([CurrentWord|Words]);
+	[Separator|Rest] -> split_words(Rest, Separator, "", [CurrentWord|Words]);
+	[Char|Rest] -> split_words(Rest, Separator, CurrentWord++[Char], Words)
+    end.
+
+add_words([], Accumulator, _Columns) ->
+    lists:reverse(Accumulator);
+add_words([Word|Rest], Accumulator, Columns) ->
+    NewAccumulator = add_word(Word, Accumulator, Columns),
+    add_words(Rest, NewAccumulator, Columns).
+
+add_word(Word, Accumulator, Columns) ->
+    [CurrentLine|Rest] = Accumulator,
+    case length(CurrentLine++" "++Word) > Columns of
+	false ->
+	    case length(CurrentLine) of
+		0 -> [Word|Rest];
+		_ -> [CurrentLine++" "++Word|Rest]
+	    end;
+	true -> [Word|[CurrentLine|Rest]]
+    end.    
 
 recv_string(Socket) ->
     inet:setopts(Socket, [{active, once}]),
