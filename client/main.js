5ef432f99b96b167225cca7a4b7766416a655222
Randy Voet <randyv03t@gmail.com>
2010-09-19 18:36:30 +0200

    drawing an svg face on the canvas (again: only works in chrome)

--- client/main.js
+++ client/main.js
@@ -10,16 +10,38 @@ function setFullWindow(obj, margin) {
     obj.style.left = margin+"px";
 }
 
+function centerWindow(obj) {
+    var mainWidth = window.innerWidth;
+    var mainHeight = window.innerHeight;
+    var width = obj.width;
+    var height = obj.height;
+    var top = (mainHeight - height) / 2;
+    var left = (mainWidth - width) / 2;
+    obj.style.position = "fixed";
+    obj.style.top = top+"px";
+    obj.style.left = left+"px";
+}
+
+function drawImageOnLoad(cxt, url, x, y) {
+    var img = new Image();
+    img.onload = function() {
+	cxt.fillStyle = "#0000FF";
+	cxt.drawImage(img, x, y);
+    };
+    img.src = url;
+}
+
 function drawCanvas() {
     var canvas = document.getElementById("maincanvas");
     var cxt = canvas.getContext("2d");
     cxt.fillStyle = "#00FF00";
-    cxt.fillRect(0, 0, canvas.width - 1, canvas.height - 1);    
+    cxt.fillRect(0, 0, canvas.width - 1, canvas.height - 1);
+    drawImageOnLoad(cxt, "images/faces/human/human01.svg", 250, 140);
 }
 
 window.onload = window.onresize = function() {
     setFullWindow(document.getElementById("background_layer0"), 0);
     setFullWindow(document.getElementById("background_layer1"), 0);
-    setFullWindow(document.getElementById("maincanvas"), 50);
+    centerWindow(document.getElementById("maincanvas"));
     drawCanvas();
 };
