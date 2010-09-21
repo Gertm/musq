34f6af99150751724c034d5a58ec7910a1a6a42d
Gert Meulyzer <cel357@gmail.com>
2010-09-21 22:25:50 +0200

    better name for the handler function because there will be more then one in the future.

--- server/httpd.go
+++ server/httpd.go
@@ -4,11 +4,11 @@ import (
 	"http"
 )
 
-func handler(c *http.Conn, r *http.Request) {
+func data_handler(c *http.Conn, r *http.Request) {
 	http.ServeFile(c, r, r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/data/", handler)
+	http.HandleFunc("/data/", data_handler)
 	http.ListenAndServe(":8080", nil)
 }
