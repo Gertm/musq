39d0e9f6011cac49277df84e1127301bc9741aa3
Randy Voet <randyv03t@gmail.com>
2010-09-23 18:53:18 +0200

    client: reorganized code so I don't have to add the namespaces when calling functions internally (using closures instead of just objects); also added a mini unit testing framework

--- client/musq.js
+++ client/musq.js
@@ -1,79 +1,148 @@
-var musq = {
+var musq = function() {
 
-    _private: {
+    //##############################################################################################
 
-	helpers: {
-	    toPx: function(i) {
-		return i + "px";
-	    }
-	},
+    var utils = function() {
 
-	resourceBuffer: function () {
+	function toPx(i) {
+	    return i + "px";
+	}
+
+	return {
+	    toPx: toPx  
+	};
+	
+    }();
+
+    //##############################################################################################
+
+    var main = function() {
+
+	var resourceBuffer = function() {
+
 	    var container = {};
+
+	    function addImage(key, url) {
+		var image = new Image();
+		image.src = url;
+		container[key] = image;
+	    }
+
+	    function addXml(key, url) {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", url, false);
+		xmlHttp.send();
+		var xmlDoc = xmlHttp.responseXML;
+		container[key] = xmlDoc;
+	    }
+
+	    function remove(key) {
+		container[key] = undefined;
+	    }
+
+	    function get(key) {
+		return container[key];
+	    }
+
 	    return {
-		addImage: function (key, url) {
-		    var image = new Image();
-		    image.src = url;
-		    container[key] = image;
-		},
-		remove: function (key) {
-		    container[key] = undefined;
-		},
-		get: function (key) {
-		    return container[key];
-		}
+		addImage: addImage,
+		addXml: addXml,
+		remove: remove,
+		get: get
 	    };
-	} (),
 
-	footerHeight: 20,
+	}();
 
-	positionCanvas: function() {
-	    var toPx = musq._private.helpers.toPx;
+	var footerHeight = 20;
+
+	function positionCanvas() {
 	    var canvas = document.getElementById("maincanvas");
 	    var footer = document.getElementById("footer");
 	    var xPadding = 40;
 	    var yPadding = 20;
 	    canvas.style.position = "fixed";
 	    var newWidth = window.innerWidth - xPadding * 2;
-	    canvas.setAttribute("width", toPx(newWidth));
-	    var newHeight = window.innerHeight - yPadding * 2 - musq._private.footerHeight;
-	    canvas.setAttribute("height", toPx(newHeight));
-	    canvas.style.top = toPx(yPadding);
-	    canvas.style.left = toPx(xPadding);
-	},
+	    canvas.setAttribute("width", utils.toPx(newWidth));
+	    var newHeight = window.innerHeight - yPadding * 2 - footerHeight;
+	    canvas.setAttribute("height", utils.toPx(newHeight));
+	    canvas.style.top = utils.toPx(yPadding);
+	    canvas.style.left = utils.toPx(xPadding);
+	}
 
-	positionFooter: function() {
-	    var toPx = musq._private.helpers.toPx;
+	function positionFooter() {
 	    var footer = document.getElementById("footer");
 	    footer.style.position = "fixed";
-	    footer.style.width = toPx(window.innerWidth);
-	    footer.style.top = toPx(window.innerHeight - musq._private.footerHeight);
-	    footer.style.left = toPx(0);
-	},
+	    footer.style.width = utils.toPx(window.innerWidth);
+	    footer.style.top = utils.toPx(window.innerHeight - footerHeight);
+	    footer.style.left = utils.toPx(0);
+	}
 
-	drawCanvas: function() {
+	function drawSvg(cxt, svg, x, y) {
+	}
+
+	function drawCanvas() {
 	    var canvas = document.getElementById("maincanvas");
 	    var cxt = canvas.getContext("2d");
 	    cxt.fillStyle = "#FFFFFF";
 	    cxt.fillRect(0, 0, canvas.width - 1, canvas.height - 1);
-	    cxt.drawImage(musq._private.resourceBuffer.get("human01"), 250, 140);
-	},
-
-	onWindowResize: function() {
-	    musq._private.positionCanvas();
-	    musq._private.positionFooter();
-	    musq._private.drawCanvas();
-	},
-
-	onWindowLoad: function() {
-	    musq._private.resourceBuffer.addImage("human01", "images/faces/human/human01.svg");
-	    musq._private.onWindowResize();
-	    setInterval(musq._private.drawCanvas, 1000);
+	    //drawSvg(cxt, resourceBuffer.get("human01"), 250, 140);
+	    cxt.drawImage(resourceBuffer.get("human01"), 250, 140);
 	}
 
-    }
+	function onWindowResize() {
+	    positionCanvas();
+	    positionFooter();
+	    drawCanvas();
+	}
 
-};
+	function onWindowLoad() {
+	    //resourceBuffer.addXml("human01", "images/faces/human/human01.svg");
+	    resourceBuffer.addImage("human01", "images/faces/human/human01.svg");
+	    onWindowResize();
+	    setInterval(drawCanvas, 1000);
+	}
 
-window.onload = musq._private.onWindowLoad;
-window.onresize = musq._private.onWindowResize;
+	return {
+	    onWindowLoad: onWindowLoad,
+	    onWindowResize: onWindowResize
+	};
+
+    }();
+
+    //##############################################################################################
+
+    var testing = function() {
+
+	function test(testString, condition) {
+	    if (!condition) {
+		alert("test failed: " + testString + "!");
+	    }
+	}
+
+	function runTests() {
+	    test("toPx", (utils.toPx(10) == "10px"));
+	}
+
+	return {
+	    runTests: runTests
+	};
+
+    }();
+
+    return {
+	main: main,
+	testing: testing
+    };
+
+}();
+
+//##################################################################################################
+
+var runtests = false;
+if (!runtests) {
+    window.onload = musq.main.onWindowLoad;
+    window.onresize = musq.main.onWindowResize;
+}
+else {
+    window.onload = musq.testing.runTests;
+}
