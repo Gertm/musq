c186476d67a31a152ca2cbd8954cbc6d14df0e5e
Gert Meulyzer <cel357@gmail.com>
2010-09-25 09:28:35 +0200

    improved the http server a bit

--- server/httpd.go
+++ server/httpd.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"http"
 )
 
+// this function serves up the 'data' folder
 func data_handler(c *http.Conn, r *http.Request) {
-	http.ServeFile(c, r, r.URL.Path[1:])
+	path := "/data/" + r.URL.Path[1:]
+	// just to see what's going on, let's put some debug logging in
+	fmt.Print("Serving file " + path + " to " + r.Host + "\n")
+	http.ServeFile(c, r, path)
 }
 
 func main() {
-	http.HandleFunc("/data/", data_handler)
+	http.HandleFunc("/", data_handler)
 	http.ListenAndServe(":8080", nil)
 }
