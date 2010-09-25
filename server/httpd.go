bb295df3204f3d5a71f773a2dc2a4b5f70e50022
Gert Meulyzer <cel357@gmail.com>
2010-09-26 00:51:18 +0200

    hosting the client folder now

--- server/httpd.go
+++ server/httpd.go
@@ -9,7 +9,7 @@ import (
 
 // this function serves up the 'data' folder
 func data_handler(c *http.Conn, r *http.Request) {
-	path := "./data/" + r.URL.Path[1:]
+	path := "../client/" + r.URL.Path[1:]
 	// just to see what's going on, let's put some debug logging in
 	fmt.Print("Serving file " + path + " to " + r.Host + "\n")
 	http.ServeFile(c, r, path)
