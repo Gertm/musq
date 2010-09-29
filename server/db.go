2e4f4d05ec640d4e6abda922fb0b1559d78b39c7
Gert Meulyzer <cel357@gmail.com>
2010-09-29 22:32:41 +0200

    aha, it seems to work now!

--- server/db.go
+++ server/db.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Player struct {
+	Name		 string
 	X			 int
 	Y			 int
-	Name		 string
 	SVG			 string
 }
 
@@ -24,23 +24,24 @@ func main() {
 	
 	fmt.Print("Inserted Player1\n")
 
-	Player2 := Player{X:1, Y:1, Name: "empty", SVG: "bla"}
-	
 	qFindDoc, err := mongo.Marshal(&map[string]string{
-		"Name": "Randy",
+		"name":"Randy",
 	})
+
 	if err != nil {
 		fmt.Print(err.String())
 	}
 	bsonP2, err := collection.FindOne(qFindDoc)
+	
 	if err != nil {
-		fmt.Print("something went wrong with the bsonP2\n")
-		fmt.Print(err.String())
+		fmt.Print(err.String()+"\n")
+		panic("something went wrong with the bsonP2\n")
 	}
 	if mongo.Equal(bsonP1, bsonP2) {
 		fmt.Print("both objects are the same!\n")
 	}
 	
+	var Player2 Player
 	mongo.Unmarshal(bsonP2.Bytes(), &Player2)
 	fmt.Print("Found player in db: "+Player2.Name+" -> "+Player2.SVG)
 	
