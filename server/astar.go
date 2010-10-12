774a27133bd3cdf6fdc71645007bb41733235bb0
Gert Meulyzer <cel357@gmail.com>
2010-10-12 17:00:21 +0200

    server: resulting paths are funky, but it'll get there. COMPILES

--- server/astar.go
+++ server/astar.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"strconv"
 )
 
 func TileDistance(x1, y1, x2, y2 int) int {
@@ -30,47 +32,61 @@ func (l *Location) Equals(p *Location) bool {
 	return false
 }
 
+func (l *Location) String() string {
+	return fmt.Sprintf("%d,%d",l.x,l.y)
+}
+
+func LocFromString(locstring string) Location {
+	fmt.Printf("locstring: %s\n",locstring)
+	strList := strings.Split(locstring,",",-1)
+	for i := range strList {
+		fmt.Printf("Locs[%d] = %s\n",i, strList[i])
+	}
+	xx, _ := strconv.Atoi(strList[0])
+	yy, _ := strconv.Atoi(strList[1])
+	fmt.Printf("Returning location{ x: %d, y: %d } for string '%s'\n",xx,yy,locstring)
+	return Location{x:xx,y:yy}
+}
+
 func (l *Location) CalcScore(start *Location, dest *Location) int {
 	G := LocationDistance(l, start)
 	H := LocationDistance(l, dest)
 	l.Score = G + H
-	fmt.Printf("for %d,%d: G=%d, H=%d\n",l.x, l.y, G, H)
+	//fmt.Printf("for %d,%d: G=%d, H=%d Score=%d\n",l.x, l.y, G, H, l.Score)
 	return l.Score
 }
 
 // this will need to look up the locations in the
 // database to see if the locs are available. For now,
 // they all are.
-func (l *Location) Neighbours() []Location {
+func (l *Location) Neighbours(path map[string]string) []Location {
 	neighbours := make([]Location, 8)
 	count := 0
-	for i:=l.x-1;i<l.x+2;i++ {
-		for j:=l.y-1;j<l.y+2;j++ {
-			if i==l.x && j==l.y {
-				continue
+	for i:=l.x-1;i<=l.x+1;i++ {
+		for j:=l.y-1;j<=l.y+1;j++ {
+			LocHere := Location{x:i, y:j, Score: 0, Parent: l}
+			if LocHere.Equals(l) {
+				// 
+			} else {
+				neighbours[count] = Location{x:i, y:j, Score: 0, Parent: l}
+				count++
 			}
-			// check if the tile is available.
-			//fmt.Printf("Adding %d,%d\n",i,j)
-			neighbours[count] = Location{x:i, y:j, Score:0, Parent:l}
-			count++
 		}
 	}
 	return neighbours
 }
 
 func findPath(start, dest Location) []Location {
-	path := make(map[*Location]Location)
-	current := Location{start.x,start.y,99999,nil}
-	current = start
+	path := make(map[string]string)
+	current := start
+	path[start.String()]=start.String()
 	for {
-		nextTile := selectBestNextTile(start,dest,current, path)
-		if len(path) == 0 {
-			path[&start] = nextTile
-		} else {
-			path[&current] = nextTile
-		}
+		nextTile := selectBestNextTile(start, dest, current, path)
+		path[current.String()] = nextTile.String()
 		current = nextTile
 		if nextTile.Equals(&dest) {
+			path[nextTile.String()] = current.String()
+			fmt.Printf("path: %s\n",path)
 			return makeLocList(path, &start)[1:]
 		}
 	}
@@ -78,38 +94,53 @@ func findPath(start, dest Location) []Location {
 	return nil
 }
 
-func selectBestNextTile(start, dest, tile Location, path map[*Location]Location) Location {
-	currentBest := Location{ tile.x, tile.y, 9999, tile.Parent }
-	var current Location
-	adjacent := tile.Neighbours()
-	for i:= 0; i < len(adjacent); i++ {
-		current = adjacent[i]
-		current.CalcScore(&start,&dest)
-		if (current.Score < currentBest.Score) {
-			_, ok := path[&current]
-			if !ok {
-				currentBest = adjacent[i]
+func selectBestNextTile(start, dest, tile Location, path map[string]string) Location {
+	current := tile
+	current.Score = 999999
+	neighbours := tile.Neighbours(path)
+	//fmt.Printf("Current score: %d\n", current.Score)
+	for i:=0;i<len(neighbours);i++ {
+		neighbours[i].CalcScore(&start,&dest)
+		//fmt.Printf("neighbours[%d]: %d,%d Score: %d\n", i, neighbours[i].x, neighbours[i].y, neighbours[i].Score)
+		if neighbours[i].Score < current.Score {
+			//fmt.Println("this one is smaller!")
+			if neighbours[i].Equals(&current) {
+				path[current.String()]=tile.String()
+				return current
 			}
+			_, ok := path[neighbours[i].String()]
+			if ok {
+				//fmt.Println("Found tile in path, skipping")
+				continue
+			}
+			current = neighbours[i]
+			//fmt.Printf("Current is now %d,%d\n",current.x,current.y)
 		}
 	}
-	fmt.Printf("going from %d,%d to %d,%d\n",tile.x,tile.y,currentBest.x,currentBest.y)
-	return currentBest
+	path[current.String()]=tile.String()
+	fmt.Printf("Returning %d,%d as next tile\n",current.x,current.y)
+	return current
 }
 
-func makeLocList(m map[*Location]Location, start *Location) []Location {
+func makeLocList(m map[string]string, start *Location) []Location {
 	resultList := make([]Location, len(m))
-	var step *Location
-	var next *Location
-	for i:=0;i<len(m);i++ {
-		if i==0 {
-			next, _ := m[start]
-			resultList[i] = next
-		} else {
-			next, _ := m[step]
-			resultList[i] = next
+	current := start.String()
+	count := 0
+	for {
+		fmt.Printf("Current: %s\n",current)
+		resultList[count] = LocFromString(current)
+		fmt.Printf("We're now at %d\n", count)
+		parent,ok := m[current]
+		if !ok {
+			fmt.Printf("current: %s not found!\n",current)
 		}
-
-		step = next
+		if parent == current {
+			return resultList
+		}
+		current = parent
+		count++
 	}
+	
+	fmt.Println("WE SHOULDN'T BE HERE!")
 	return resultList
 }
