25d310fc865bbf374fa15786654013fc9ec59ee6
Gert Meulyzer <cel357@gmail.com>
2010-11-15 22:05:33 +0100

    server: removed playingfield, added area

---
+++ server/area.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "os"
+    "json"
+)
+
+type Tile struct {
+    X          int
+    Y          int
+    Images     []string
+    Properties map[string]string
+}
+
+type Area struct {
+    Name        string
+    DefaultTile Tile
+    BorderTile  Tile
+    Width       int
+    Height      int
+    Tiles       []Tile
+}
+
+// load the area from disk
+
+func loadArea(filename string) (Area, os.Error) {
+    f, err := os.Open(filename, os.O_RDONLY, 0)
+    if err != nil {
+        return Area{}, err
+    }
+    d := json.NewDecoder(f) // zomg, awesome :D
+    a := Area{}
+    err = d.Decode(a)
+    if err != nil {
+        return Area{}, err
+    }
+    return a, nil
+}
