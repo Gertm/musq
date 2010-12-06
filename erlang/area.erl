375f3e48e3037d6a0adaa2d11e38ef357c7e4dc6
Gert Meulyzer <cel357@gmail.com>
2010-12-06 17:54:31 +0100

    erlang port: added area stub

---
+++ erlang/area.erl
@@ -0,0 +1,14 @@
+%%% @author Gert <@G3rtm on Twitter>
+%%% @copyright (C) 2010, Gert
+%%% @doc
+%%%
+%%% @end
+%%% Created :  6 Dec 2010 by Gert <@G3rtm on Twitter>
+
+-module(area).
+-include("musq.hrl").
+-compile(export_all).
+
+start() ->
+	ok.
+
