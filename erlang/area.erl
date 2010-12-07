9c23fa9bbc9ab7336ff9ae405994568596c99f4f
Gert Meulyzer <cel357@gmail.com>
2010-12-07 20:49:45 +0100

    erlang port: loading area file

--- erlang/area.erl
+++ erlang/area.erl
@@ -9,6 +9,25 @@
 -include("musq.hrl").
 -compile(export_all).
 
-start() ->
+start(FileName) ->
 	ok.
 
+
+loop(AreaState) ->
+	ok.
+
+
+load(FileName) ->
+	A = lists:flatten(readlines(FileName)),
+	mochijson:decode(A).
+
+
+readlines(FileName) ->
+    {ok, Device} = file:open(FileName, [read]),
+    get_all_lines(Device, []).
+
+get_all_lines(Device, Accum) ->
+    case io:get_line(Device, "") of
+        eof  -> file:close(Device), lists:reverse(Accum);
+        Line -> get_all_lines(Device, [Line|Accum])
+    end.
